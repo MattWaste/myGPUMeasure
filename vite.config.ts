62c46f115037f508444e76d0f44da7b916c31588
Matt West <weststern@gmail.com>
2025-02-18 12:49:03 -0500

    updated vite proxy file

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   define: {
-    'process.env': {}
+    'process.env': {},
   },
   plugins: [react()],
   server: {
     port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
