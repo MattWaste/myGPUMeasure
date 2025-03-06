8abecbd436ddfc6e0f8e8815499ad2137aae46a8
Matt West <weststern@gmail.com>
2025-03-06 16:19:19 -0500

    updated ecosystem config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,20 @@
 module.exports = {
-    apps: [{
-      name: "mygpupower",
-      script: "./dist/index.js",
-      env: {
-        NODE_ENV: "production",
-        PORT: 3000,
-        DATABASE_URL: "postgresql://postgres:postgres@localhost:5432/postgres"
-      },
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      log_date_format: "YYYY-MM-DD HH:mm:ss",
-      error_file: "logs/err.log",
-      out_file: "logs/out.log",
-      merge_logs: true
-    }]
-  };
+  apps: [{
+    name: "mygpupower",
+    script: "./dist/server/index.js",
+    env: {
+      NODE_ENV: "production",
+      PORT: 3000,
+      DATABASE_URL: "postgres://postgres:password@localhost:5432/postgres",
+      DB_SSL: "false"
+    },
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    log_date_format: "YYYY-MM-DD HH:mm:ss",
+    error_file: "logs/err.log",
+    out_file: "logs/out.log",
+    merge_logs: true
+  }]
+};
